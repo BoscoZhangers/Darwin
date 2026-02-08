3d833860a93c2ce7a9e67ad60fe980bb6602cc93
Lawrence Zou <46764863+BrightLaw9@users.noreply.github.com>
2026-02-08 03:46:07 -0500

    Model

--- train/neural_net.py
+++ train/neural_net.py
@@ -1,34 +1,35 @@
 import torch
 import torch.nn as nn
 
-FEATURES = ['x', 'y', 'div_category', 'border-width', 'top', 'left', 'color_R',
-       'color_G', 'color_B', 'border-color_R', 'border-color_G',
-       'border-color_B', 'background-color_R', 'background-color_G',
-       'background-color_B', 'border-style_dashed', 'border-style_double',
-       'border-style_solid', 'position_absolute', 'cursor_pointer']
-NUM_FEATURES = len(FEATURES)
+# FEATURES = 
+# NUM_FEATURES = len(FEATURES)
 
 NUM_DIVS = 5
 EMBEDDING_DIM = 256
-INNER_LAYER_SIZE = 128
+INNER_LAYER_SIZE = 256
+HIDDEN_LAYERS = 16
 
 
 class Predictor(nn.Module):
-    def __init__(self):
+    def __init__(self, num_features):
         super().__init__()
 
         #self.div_embedding = nn.Embedding(NUM_DIVS, EMBEDDING_DIM)
 
-        self.net = nn.Sequential(
-            #nn.Linear(EMBEDDING_DIM, INNER_LAYER_SIZE),
-            nn.Linear(NUM_FEATURES, INNER_LAYER_SIZE),
-            nn.ReLU(),
-            nn.Linear(INNER_LAYER_SIZE, INNER_LAYER_SIZE),
-            nn.ReLU(),
-            nn.Linear(INNER_LAYER_SIZE, INNER_LAYER_SIZE),
-            nn.ReLU(),
-            nn.Linear(INNER_LAYER_SIZE, 1) # Output is clicks
-        )
+        layers = []
+
+        # Input layer
+        layers.append(nn.Linear(num_features, INNER_LAYER_SIZE))
+        layers.append(nn.ReLU())
+
+        for _ in range(HIDDEN_LAYERS):
+            layers.append(nn.Linear(INNER_LAYER_SIZE, INNER_LAYER_SIZE))
+            layers.append(nn.ReLU())
+
+        # Output layer
+        layers.append(nn.Linear(INNER_LAYER_SIZE, 1))
+
+        self.net = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.net(x)
