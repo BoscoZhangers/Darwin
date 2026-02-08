c04bfeceee764c6ec46fc36e64173b4d6454e6ef
BoscoZhangers <168551399+BoscoZhangers@users.noreply.github.com>
2026-02-08 04:15:21 -0500

    fixed

--- api.py
+++ api.py
@@ -3,8 +3,7 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 import random
-# from train.model_sampler import Sampler
-from data.model_agent import nn_model
+from train.model_sampler import Sampler
 
 app = FastAPI()
 
@@ -21,18 +20,16 @@ app.add_middleware(
 class HitRequest(BaseModel):
     x: Optional[float]
     y: Optional[float]
-    div_id: Optional[str]
+    div_id: Optional[int]
 
 class RunPipelineRequest(BaseModel):
     html: str
 
-#sampler = Sampler()
+sampler = Sampler()
 
 @app.post('/api/get_hit_count')
 async def get_hit_count(body: HitRequest):
-    #count = int(sampler.sample(body.x, body.y, body.div_id))
-    # predict_other
-    count = int(nn_model(predict_x=body.x, predict_y=body.y, predict_id=body.div_id))
+    count = int(sampler.sample(body.x, body.y, body.div_id))
     print(count)
     return {"count": count}
 
