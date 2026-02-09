46d2b3196eda346eeaa196d8ba55633451d8302a
Lawrence Zou <46764863+BrightLaw9@users.noreply.github.com>
2026-02-08 19:08:13 -0500

    Error to coerce

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,5 +1,5 @@
 const HOST = "localhost";
 // export const APP_HOST = "http://localhost";
 export const APP_HOST = "https://darwin-mg4d.onrender.com"
-export const PORT = ":8001";
-export const BACKEND_PORT = ":8001";
+export const PORT = "";
+export const BACKEND_PORT = ""; // NO PORTS FOR PROD
